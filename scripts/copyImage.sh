3e62502d8f869fc457b41f56f92aee87aad64c71
JetsonHacksNano <jim@jetsonhacks.com>
2019-07-20 19:19:09 -0700

    cleanup copyImage comments

--- scripts/copyImage.sh
+++ scripts/copyImage.sh
@@ -4,16 +4,16 @@ echo "Source Target: ""$SOURCE_TARGET"kernel/kernel-$KERNEL_RELEASE/arch/arm64/b
 echo "Boot Path: ""$BOOT_TARGET"Image
 
 cd "$SOURCE_TARGET"kernel/kernel-$KERNEL_RELEASE
-# On the stock Jetson TX2 install, there is no zImage in the boot directory
+# On the stock Jetson Nano install, there is no zImage in the boot directory
 # So we just copy the Image file over
 # If the zImage is needed, you must either
 # $ make zImage
 # or
 # $ make
-# Both of these commands must be executed in /usr/src/kernel/kernel-4.4
+# Both of these commands must be executed in /usr/src/kernel/kernel-4.9
 # sudo cp arch/arm64/boot/zImage /boot/zImage
-# Note that if you are compiling on an external device, like a SSD, you should probably
-# copy this over to the internal eMMC if that is where the Jetson boots
+# Note that if you are compiling on an external device, like a SSD, you should
+# copy this over to the boot device (i.e SD Card) where the Jetson boots
 sudo cp arch/arm64/boot/Image "$BOOT_TARGET"Image
 
 
